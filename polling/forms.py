b00484b346ca192a3bfdb7158689b0df7c1f63ae
Édio Cerati Neto <edioceratineto@gmail.com>
2020-07-28 13:11:55 -0300

    Refatorei o formulário para criação de polls

--- polling/forms.py
+++ polling/forms.py
@@ -1,18 +1,21 @@
 from django import forms 
 
 class PollForm(forms.Form):
-    option01 = forms.CharField(label='', required=True, max_length=100, 
+    title = forms.CharField(label='Title', required=True, max_length=250, 
         widget=forms.TextInput(
-            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll option'}))
+            attrs={'class' : 'form-control form-control-sm mb-4', 'placeholder' : 'Poll title'}))
+    option01 = forms.CharField(label='Answers:', required=True, max_length=100, 
+        widget=forms.TextInput(
+            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll answer', 'id': 'option01'}))
     option02 = forms.CharField(label='', required=False, max_length=100, 
         widget=forms.TextInput(
-            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll option'}))
+            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll answer', 'id': 'option02'}))
     option03 = forms.CharField(label='', required=False, max_length=100, 
         widget=forms.TextInput(
-            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll option'}))
+            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll answer', 'id': 'option03'}))
     option04 = forms.CharField(label='', required=False, max_length=100, 
         widget=forms.TextInput(
-            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll option'}))
+            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll answer', 'id': 'option04'}))
     option05 = forms.CharField(label='', required=False, max_length=100, 
         widget=forms.TextInput(
-            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll option'}))
+            attrs={'class' : 'form-control form-control-sm mb-2', 'placeholder' : 'Add poll answer', 'id': 'option05'}))
