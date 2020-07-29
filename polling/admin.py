39a369fc614dbd0c494cf449c6ca4d9be178d7e0
Édio Cerati Neto <edioceratineto@gmail.com>
2020-07-29 14:39:34 -0300

    Refatorando o models para permitir salvar os votos

--- polling/admin.py
+++ polling/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice, User, Vote
 
 admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(User)
+admin.site.register(Vote)
--- polling/models.py
+++ polling/models.py
@@ -3,14 +3,24 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=150)
     free_answers = models.BooleanField(default=False)
-    
+    launched = models.BooleanField(default=False)
+
     def __str__(self):
         return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=80)
-    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
+class User(models.Model):
+    username = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.username
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
