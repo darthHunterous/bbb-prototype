3d1c540e681e6d4f6f4016a5c18d4255d2c29b60
allanamancio <allanamancio.rocha@gmail.com>
2020-07-29 01:17:51 -0300

    Opção de anonimidade adicionada

--- polling/models.py
+++ polling/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=150)
     free_answers = models.BooleanField(default=False)
-    
+    anonymous_answers = models.BooleanField(default=False)
+
     def __str__(self):
         return self.question_text
 
--- polling/views.py
+++ polling/views.py
@@ -4,7 +4,7 @@ from .models import Question
 from .forms import PollForm
 
 def index(request):
-    polls = Question.objects.order_by('id')	
+    polls = Question.objects.order_by('id')
     context = { 'polls': polls }
 
     return render(request, 'polling/index.html', context)
@@ -14,8 +14,14 @@ def create(request):
         form = PollForm(request.POST)
 
         if form.is_valid():
+            # Se o anonymous_answers for 'on', significa True
+            anonymous_answers_fixed=request.POST.get('anonymousAnswers', False)
+            if (anonymous_answers_fixed != False):
+                anonymous_answers_fixed = True
+
             new_question = Question(question_text=request.POST['title'],
-                                    free_answers=request.POST.get('freeAnswers', False))
+                                    free_answers=request.POST.get('freeAnswers', False),
+                                    anonymous_answers=anonymous_answers_fixed)
             new_question.save()
 
             if request.POST['option01'] != '':
